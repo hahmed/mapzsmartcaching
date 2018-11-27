ed8ddf7203d1ef2839984c96a5954681b7d9adf9
Haroon Ahmed <haroon.ahmed25@gmail.com>
2018-11-27 09:17:53 +0000

    add country migration, add geohashing

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,27 @@
+require "pr_geohash"
+
 class Venue < ApplicationRecord
+  geocoded_by :address
+  # get the country from geocoder, then we can use the cache_geohash method to save country in cache
+  # using the geohash
+  after_validation :geocode
+
+  validates :address, presence: true
+
+  before_save :cache_geohash
+
+  def self.find_country_using_geohash_from_cache(lat, long)
+    Rails.cache.fetch("geohash:#{geohash(lat, long)}")
+  end
+
+  def self.geohash(lat, long)
+    return '' unless lat && long
+    GeoHash.encode(lat, long, 6)
+  end
+
+  private
+
+  def cache_geohash
+    Rails.cache.write("geohash:#{Venue.geohash(latitude, longitude)}", 'France') # country
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_163031) do
+ActiveRecord::Schema.define(version: 2018_11_26_234926) do
 
   create_table "venues", force: :cascade do |t|
     t.string "address"
@@ -18,6 +18,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_163031) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country"
   end
-
 end
